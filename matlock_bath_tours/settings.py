3ea3a62032f466e41603b4e6741825d8b10c9135
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-05-18 11:14:52 +0100

    Add tours app, model and manually add data

--- matlock_bath_tours/settings.py
+++ matlock_bath_tours/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'tours',
 ]
 
 MIDDLEWARE = [
@@ -150,6 +151,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
