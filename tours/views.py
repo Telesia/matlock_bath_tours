140ac2c27526b7ad116229a94c554f56c6bec3c0
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-05-18 11:54:35 +0100

    Add tours.html and render example

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Tour
 
-# Create your views here.
+
+def all_tours(request):
+    """ A view to show all tours, including sorting and search queries"""
+
+    tours = Tour.objects.all()
+
+    context = {
+        'tours': tours,
+    }
+
+    return render(request, 'tours/tours.html', context)
