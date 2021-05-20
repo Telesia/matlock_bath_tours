1c5c4b7e14b589872d5dde4e9caf1165afa57fac
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-05-20 20:00:04 +0100

    Restructure index.html to include a hero video header Add seperate navbar.html file and include, wire up searchbar form to redirect to all_tours url view

--- tours/views.py
+++ tours/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from audioop import reverse
+from pyexpat.errors import messages
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from .models import Tour
 
 
@@ -6,6 +10,13 @@ def all_tours(request):
     """ A view to show all tours, including sorting and search queries"""
 
     tours = Tour.objects.all()
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please search for a walking tour!")
+                return redirect(reverse('all_tours'))
 
     context = {
         'tours': tours,
