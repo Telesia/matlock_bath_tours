9719136b0baca2a419f2f1807aa82c17f28b1fae
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-05-23 13:17:57 +0100

    Add sorting feature to navbar.html and in tours view

--- tours/views.py
+++ tours/views.py
@@ -11,8 +11,22 @@ def all_tours(request):
 
     tours = Tour.objects.all()
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                tours = tours.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f' -{sortkey}'
+            tours = tours.order_by(sortkey)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -22,9 +36,12 @@ def all_tours(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             tours = tours.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'tours': tours,
         'search_terms': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'tours/tours.html', context)
