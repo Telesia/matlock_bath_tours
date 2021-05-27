6543697fef772e03dbf636c9babf6b6e26b7a6f9
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-05-27 19:47:03 +0100

    Add to contexts.py functionality to add items to bag and total price

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,23 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from tours.models import Tour
 
 
 def bag_contents(request):
     bag_items = []
     total = 0
     tour_count = 0
+    bag = request.session.get('bag', {})
+
+    for tour_id, quantity in bag.items():
+        tour = get_object_or_404(Tour, pk=tour_id)
+        total += quantity * tour.price
+        tour_count += quantity
+        bag_items.append({
+            'tour_id': tour_id,
+            'quantity': quantity,
+            'tour': tour,
+        })
 
     context = {
         'bag_items': bag_items,
