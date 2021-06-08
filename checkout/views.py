079109dbe9434c87976b31253d8b5c4a2e78bc25
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-06-08 08:42:18 +0100

    Add checkout css directories and file

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    # Request bag contents from session
+    bag = request.session.get('bag', {})
+
+    # If nothing in bag add an error message
+    if not bag:
+        messages.error(request, "There's nothing in your bag")
+        return redirect(reverse('products'))
+
+    # Create an instance of the OrderForm from forms.py
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
