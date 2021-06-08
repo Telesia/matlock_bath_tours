af984b39b408af083858114951e473ea1221eda5
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-06-08 10:55:46 +0100

    Wire up remove_from_bag functionality to template

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from tours.models import Tour
 from django.contrib import messages
 
@@ -30,13 +30,16 @@ def add_to_bag(request, tour_id):
 def adjust_bag(request, tour_id):
     """ Adjust the quantity of tours in the shopping bag """
 
+    tour = get_object_or_404(Tour, pk=tour_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[tour_id] = quantity
+        messages.success(request, f'Updated {tour.name} quantity to {bag[tour_id]}')
     else:
         bag.pop(tour_id)
+        messages.success(request, f'Removed {tour.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -46,11 +49,24 @@ def adjust_bag(request, tour_id):
 def remove_from_bag(request, tour_id):
     """ Remove tour from the shopping bag """
     try:
+        product = get_object_or_404(Tour, pk=tour_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
-        bag.pop(tour_id)
+        if size:
+            del bag[tour_id]['items_by_size'][size]
+            if not bag[tour_id]['items_by_size']:
+                bag.pop(tour_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+        else:
+            bag.pop(tour_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
+
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
