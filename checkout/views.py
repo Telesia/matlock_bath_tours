fe81190d427911cd1e22398b3c869552a66a507c
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2021-06-20 15:58:21 +0100

    Add logic for stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     # Request bag contents from session
     bag = request.session.get('bag', {})
 
@@ -13,13 +20,30 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag")
         return redirect(reverse('all_tours'))
 
+    # Create a new bag total amount for stripe payment
+    current_bag = bag_contents(request)
+    total = current_bag['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     # Create an instance of the OrderForm from forms.py
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
